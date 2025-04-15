b8de1ddf5cefc34ab3d2a626b05a7bd45c6ed4db
nguyenson2012 <traibachkhoa2012@gmail.com>
2025-04-15 22:25:22 +0700

    15-04: Update to use jpa and H2 database

--- src/main/java/com/nguyenson/ecommerce/nguyenson_ecommerce/UserService.java
+++ src/main/java/com/nguyenson/ecommerce/nguyenson_ecommerce/UserService.java
@@ -1,38 +1,35 @@
 package com.nguyenson.ecommerce.nguyenson_ecommerce;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    private final List<User> userList = new ArrayList<>();
-    private Long nextId = 1L;
+    UserRepository userRepository;
 
     public List<User> fetchAllUsers(){
-        return userList;
+        return userRepository.findAll();
     }
 
     public void addUser(User user){
-        user.setId(nextId++);
-        userList.add(user);
+        userRepository.save(user);
     }
 
     public Optional<User> fetchUser(Long id) {
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst();
+        return userRepository.findById(id);
     }
 
     public boolean updateUser(Long id, User updatedUser) {
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst()
+        return userRepository.findById(id)
                 .map(existingUser -> {
                     existingUser.setFirstName(updatedUser.getFirstName());
                     existingUser.setLastName(updatedUser.getLastName());
+                    userRepository.save(existingUser);
                     return true;
                 }).orElse(false);
     }
