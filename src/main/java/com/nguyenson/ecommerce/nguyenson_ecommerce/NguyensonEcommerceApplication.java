daf207bdef741f2109da483c10f41956df577aff
nguyenson2012 <traibachkhoa2012@gmail.com>
2025-04-15 21:34:12 +0700

    first commit

---
+++ src/main/java/com/nguyenson/ecommerce/nguyenson_ecommerce/NguyensonEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.nguyenson.ecommerce.nguyenson_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyensonEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyensonEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nguyenson/ecommerce/nguyenson_ecommerce/NguyensonEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenson.ecommerce.nguyenson_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyensonEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
