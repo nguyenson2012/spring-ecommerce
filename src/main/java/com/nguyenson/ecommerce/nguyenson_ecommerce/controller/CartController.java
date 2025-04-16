2b82591d0cf44fcd33c7b3b494ae94cb3506fb9b
nguyenson2012 <traibachkhoa2012@gmail.com>
2025-04-16 21:47:14 +0700

    16-04: Add CartController

---
+++ src/main/java/com/nguyenson/ecommerce/nguyenson_ecommerce/controller/CartController.java
@@ -0,0 +1,46 @@
+package com.nguyenson.ecommerce.nguyenson_ecommerce.controller;
+
+import com.nguyenson.ecommerce.nguyenson_ecommerce.dto.CartItemRequest;
+import com.nguyenson.ecommerce.nguyenson_ecommerce.model.CartItem;
+import com.nguyenson.ecommerce.nguyenson_ecommerce.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cart")
+@RequiredArgsConstructor
+public class CartController {
+    @Autowired
+    CartService cartService;
+
+    @PostMapping
+    public ResponseEntity<String> addToCart(
+            @RequestHeader("X-User-ID") String userId,
+            @RequestBody CartItemRequest request) {
+        if (!cartService.addToCart(userId, request)) {
+            return ResponseEntity.badRequest().body("Product Out of Stock or User not found or Product not found");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/items/{productId}")
+    public ResponseEntity<Void> removeFromCart(
+            @RequestHeader("X-User-ID") String userId,
+            @PathVariable Long productId) {
+        boolean deleted = cartService.deleteItemFromCart(userId, productId);
+        return deleted ? ResponseEntity.noContent().build()
+                : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CartItem>> getCart(
+            @RequestHeader("X-User-ID") String userId) {
+        return ResponseEntity.ok(cartService.getCart(userId));
+    }
+
+}
